066dcf7510b8f5760d4a08a7229e7ccfec8f4bcd
amineen64 <amineen64@gmail.com>
2025-03-15 14:49:56 +0000

    implemented a counter struct

--- server.go
+++ server.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+type Counter struct {
+	mu    sync.Mutex
+	value int
+}
+
+func (c *Counter) Increment(value int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value += value
+}
 
 func main() {
-	msg := "Welcome to the Go programming"
-	fmt.Println(msg)
+	var wg sync.WaitGroup
+	counter := Counter{}
+	numGoroutines := 100
+	wg.Add(numGoroutines)
+	for i := 0; i < numGoroutines; i++ {
+		go func() {
+			defer wg.Done()
+			counter.Increment(2)
+		}()
+	}
+	wg.Wait()
+	fmt.Println("Final counter value:", counter.value)
 }
