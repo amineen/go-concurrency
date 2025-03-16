34eed2c3872ca723d07381d9e1c66f9cfc4618c3
amineen64 <amineen64@gmail.com>
2025-03-16 18:50:52 +0000

    unblocking multiple goroutines

--- channels/main.go
+++ channels/main.go
@@ -1,17 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	numberStream := make(chan int)
 
 	go func() {
 		defer close(numberStream)
-		for i := range 5 {
+		for i := range 2 {
 			numberStream <- i
 		}
 	}()
 	for x := range numberStream {
 		fmt.Printf("Received: %d\n", x)
 	}
+
+	begin := make(chan any)
+	var wg sync.WaitGroup
+
+	for i := range 5 {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			<-begin
+			fmt.Printf("%v has begun\n", id)
+		}(i)
+	}
+	fmt.Println("Unblocking goroutines...")
+	
+	close(begin)
+	wg.Wait()
+
 }
